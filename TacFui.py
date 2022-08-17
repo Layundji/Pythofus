e5408d6cbf65a46ca4cbdc28eb480acbcb31ff06
Layundji <65718194+Layundji@users.noreply.github.com>
2022-08-17 15:27:25 +0200

    Add files via upload

--- TacFui.py
+++ TacFui.py
@@ -13,6 +13,13 @@ def CalcOptimalLock(dodge, init_p, final_p) :
     if tac.is_integer() and final_p%2: tac+=1
     tac = math.ceil(tac)
     return int(tac)
+def CalcOptimalLock_Wrapper(dodge, init_p) :
+    def CalcOptimalLock_Nested(final_p) :
+        tac = (init_p * (dodge+2))/((final_p+0.5)*2) -2
+        if tac.is_integer() and final_p%2: tac+=1
+        tac = math.ceil(tac)
+        return int(tac)
+    return CalcOptimalLock_Nested
 
 def CalcOptimalDodge(lock, init_p, final_p):
     """Renvoie la valeur de fuite limite pour detacler un opposant en concervant un nombre de points donnes
@@ -23,8 +30,15 @@ def CalcOptimalDodge(lock, init_p, final_p):
     """
     fui = (final_p-0.5)*(2*(lock+2))/(init_p) -2
     if fui.is_integer() and final_p%2 : fui += 1
-    fui = math.ceil(fui)
+    fio = math.ceil(fui)
     return int(fui)
+def CalcOptimalDodge_Wrapper(lock, init_p) :
+    def CalcOptimalDodge_Nested(final_p) :
+        fui = (final_p-0.5)*(2*(lock+2))/(init_p) -2
+        if fui.is_integer() and final_p%2 : fui += 1
+        fio = math.ceil(fui)
+        return int(fui)
+    return CalcOptimalDodge_Nested
 
 def CalcRemainingP(dodge, lock, init_p) :
     """ Renvoie le nombre de points apres detacle
@@ -42,29 +56,17 @@ def CalcRemainingP(dodge, lock, init_p) :
 if __name__ == '__main__' :
 
     pm_init = 6
+    tacle = 70
+    fuite = 70
 
-    print()
-    print("#"*25)
-    print()
+    p = list(range(pm_init+1))
 
-    dodge = 70
-    print(f"PM initiaux : {pm_init}")
-    print(f"fuite : {dodge}")
+    OptiTac = CalcOptimalLock_Wrapper(dodge = fuite, init_p = pm_init)
+    OptiFui = CalcOptimalDodge_Wrapper(lock = tacle, init_p = pm_init)
 
-    print("PM detacle".rjust(12),"Tacle".rjust(8),sep='')
-    for pm_end in range(pm_init+1) :
-        opti_lock = CalcOptimalLock(dodge, pm_init, pm_end)
-        print(f"{pm_end:>12}{opti_lock:>8}")
+    opti_tac = list(map(OptiTac,p))
+    opti_fui = list(map(OptiFui,p))
 
-    print()
-    print("#"*25)
-    print()
-
-    lock = 70
-    print(f"PM initiaux : {pm_init}")
-    print(f"tacle : {lock}")
-
-    print("PM detacle".rjust(12),"Fuite".rjust(8),sep='')
-    for pm_end in range(pm_init+1) :
-        opti_dodge = CalcOptimalDodge(lock, pm_init, pm_end)
-        print(f"{pm_end:>12}{opti_dodge:>8}")
+    print(f"PM finaux : {p}")
+    print(f"fuite opti: {opti_fui}")
+    print(f"tacle opti: {opti_tac}")
